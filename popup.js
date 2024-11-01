b30a7f6932763b31338547a09ec8b5ae3dd06e76
hazik-khalid <hazikkhalid111@gmail.com>
2024-11-01 17:59:58 +0530

    using google api

--- popup.js
+++ popup.js
@@ -3,3 +3,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const bookmarkContainer = document.getElementById('bookmark-container')
     const saveButton = document.getElementById('save-bookmark')
 })
+
+//Load Bookmarks
+
+
+//Save Current Page as Bookmark
+saveButton.addEventListener('click', async ()=>{
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    
+})
+
+//Function To Load bookmarks
+function loadBookmarks(){
+
+}
