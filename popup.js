c8f4c1528687d50da4ccbb0795fdce5dfd58cb56
hazik-khalid <hazikkhalid111@gmail.com>
2024-11-01 18:09:02 +0530

    display from localstorage

--- popup.js
+++ popup.js
@@ -35,3 +35,21 @@ function saveBookmark(bookmark) {
         chrome.storage.local.set({ bookmarks }, loadBookmarks);
     });
 }
+
+//Display From Local Storage
+function displayBookmark(bookmark) {
+    const bookmarkDiv = document.createElement('div');
+    bookmarkDiv.className = 'bookmark';
+
+    const img = document.createElement('img');
+    img.src = bookmark.thumbnailUrl;
+    img.alt = `${bookmark.title} thumbnail`;
+    img.className = 'thumbnail';
+
+    const title = document.createElement('p');
+    title.textContent = bookmark.title;
+    title.className = 'title';
+    bookmarkDiv.appendChild(img);
+    bookmarkDiv.appendChild(title);
+    bookmarkContainer.appendChild(bookmarkDiv);
+}
--- test.js
+++ test.js
@@ -10,21 +10,5 @@
 
 
     // Function to display a bookmark
-    function displayBookmark(bookmark) {
-        const bookmarkDiv = document.createElement('div');
-        bookmarkDiv.className = 'bookmark';
 
-        const img = document.createElement('img');
-        img.src = bookmark.thumbnailUrl;
-        img.alt = `${bookmark.title} thumbnail`;
-        img.className = 'thumbnail';
-
-        const title = document.createElement('p');
-        title.textContent = bookmark.title;
-        title.className = 'title';
-
-        bookmarkDiv.appendChild(img);
-        bookmarkDiv.appendChild(title);
-        bookmarkContainer.appendChild(bookmarkDiv);
-    }
 
