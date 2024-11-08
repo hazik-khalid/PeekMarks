5155174581c3b5030320f3d7274e938590b5a3a2
hazik-khalid <hazikkhalid111@gmail.com>
2024-11-08 19:03:56 +0530

    final commit

--- appwriteConfig.js
+++
@@ -1,9 +0,0 @@
-import { Client, Account, Databases } from "appwrite";
-
-// Initialize Appwrite
-const client = new Client()
-  .setEndpoint("https://[APPWRITE_ENDPOINT]") // Your Appwrite endpoint
-  .setProject("[PROJECT_ID]"); // Your Appwrite project ID
-
-export const account = new Account(client);
-export const databases = new Databases(client);
